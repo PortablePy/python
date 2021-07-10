beafef6f4280fdeacc31001575d8c13b70039985
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2021-07-10 10:52:22 +0530

    json

--- 0015/Json.py
+++ 0015/Json.py
@@ -2,8 +2,8 @@
 #அச்சிடு வரையறு - பின் வரும் நிரல்களில் அச்சிடு பயன்படுத்தலாம்.
 def அச்சிடு(*வாதங்கள்,பிரி=" ",முடி='\n',கோப்பு=None,பறிப்பு=False):
     print(*வாதங்கள், sep=பிரி,end=முடி, file=கோப்பு,flush=பறிப்பு)
-    
-# உள்ளீடு வரையறு - பின் வரும் நிரல்களில் அச்சிடு பயன்படுத்தலாம்.    
+
+# உள்ளீடு வரையறு - பின் வரும் நிரல்களில் அச்சிடு பயன்படுத்தலாம்.
 def உள்ளீடு(*வாதங்கள்):
     அ = input (*வாதங்கள்)
     return அ
@@ -11,17 +11,17 @@ def உள்ளீடு(*வாதங்கள்):
 # https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224
 
 import urllib.request
-import json
-import textwrap
+import json as சாகைபொகு
+import textwrap as உரைமடக்கு
 
-with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224") as f:
-    text = f.read()
-    decodedtext = text.decode('utf-8')
-    அச்சிடு(textwrap.fill(decodedtext, width=50))
+with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224") as கோப்பு:
+    உரை = கோப்பு.read()
+    குறியீட்டு_நீக்கப்பட்ட_உரை = உரை.decode('utf-8')
+    அச்சிடு(உரைமடக்கு.fill(குறியீட்டு_நீக்கப்பட்ட_உரை, width=50))
 
 அச்சிடு()
 
-obj = json.loads(decodedtext)
-அச்சிடு(obj['kind'])
+பொருள் = சாகைபொகு.loads(குறியீட்டு_நீக்கப்பட்ட_உரை)
+அச்சிடு(பொருள்['kind'])
 
-அச்சிடு(obj['items'][0]['searchInfo']['textSnippet'])
+அச்சிடு(பொருள்['items'][0]['searchInfo']['textSnippet'])
